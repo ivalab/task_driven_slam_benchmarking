9830ed967c8dc71adfd2bddc0a9bba2f225d0c4a
Yanwei Du <yanwei.du@gatech.edu>
2024-01-03 21:42:34 -0500

    Added robot exploring script

---
+++ scripts/closedloop_nav_slam/ros/explore.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@file explore.py
+@author Yanwei Du (yanwei.du@gatech.edu)
+@date 12-13-2023
+@version 1.0
+@license Copyright (c) 2023
+@desc None
+'''
+
+"""
+This code is originally borrowed 
+from https://github.com/YaelBenShalom/Turtlebot3-Navigation-with-SLAM/blob/master/nodes/explore.py
+with our own modifications.
+"""
+
+import rospy
+import actionlib
+import numpy as np
+from actionlib_msgs.msg import GoalStatus
+from move_base_msgs.msg import MoveBaseFeedback, MoveBaseGoal, MoveBaseAction
+from nav_msgs.msg import OccupancyGrid, Odometry, MapMetaData
+from geometry_msgs.msg import Pose, Point, Quaternion
+from random import randrange
+import time
+
+class Explore:
+
+    def __init__(self):
+        """ Initialize environment
+        """
+        # Initialize rate:
+        self.rate = rospy.Rate(1)
+
+        # Simple Action Client:
+        self.move_base = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        self.move_base.wait_for_server(rospy.Duration(5.0))
+        rospy.logdebug("move_base is ready") 
+
+        self.goal = []
+        self.completion = 0
+        self.pose = []
+
+        # Initialize subscribers:
+        self.map_frame = rospy.get_param("map_frame", "map")
+        self.map = OccupancyGrid()
+        self.map_metadata = None
+        self.sub_map = rospy.Subscriber('/map', OccupancyGrid, self.map_callback)
+        self.sub_map_metadata = rospy.Subscriber('/map_metadata', MapMetaData, self.map_metadata_callback)
+        self.sub_odom = rospy.Subscriber('/odom', Odometry, self.odom_callback)
+        self.count = 0
+        time.sleep(8)
+
+
+    def map_callback(self, data):
+        """ Callback function for map subscriber.
+        Subscribes to /map to get the OccupancyGrid of the map.
+        """
+        if self.map_metadata is None:
+            return
+
+        valid = False
+
+        while valid is False:
+            map_size = randrange(len(data.data))
+            self.map = data.data[map_size]
+
+            edges = self.check_neighbors(data, map_size)
+            if self.map != -1 and self.map <= 0.2 and edges is True:
+                valid = True
+            
+        row = map_size / self.map_metadata.width
+        col = map_size % self.map_metadata.width
+
+        gx = col * self.map_metadata.resolution + self.map_metadata.origin.position.x  # column * resolution + origin_x
+        gy = row * self.map_metadata.resolution + self.map_metadata.origin.position.y  # row * resolution + origin_x
+        self.goal = [gx, gy]
+        print(self.goal)
+        
+        if self.completion % 2 == 0:
+            self.completion += 1
+            # Start the robot moving toward the goal
+            self.set_goal()
+
+    def map_metadata_callback(self, msg):
+        self.map_metadata = msg
+
+    def odom_callback(self, msg):
+        self.pose = msg.pose.pose
+
+    def set_goal(self):
+        """ Set goal position for move_base.
+        """
+        rospy.logdebug("Setting goal")
+
+        # Create goal:
+        goal = MoveBaseGoal()
+
+        # Set random goal:
+        goal.target_pose.header.frame_id = self.map_frame
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = self.goal[0]
+        goal.target_pose.pose.position.y = self.goal[1]
+        goal.target_pose.pose.orientation.w = 1.0
+        rospy.logdebug(f"goal: {goal.target_pose.pose.position.x, goal.target_pose.pose.position.y}")
+        self.move_base.send_goal(goal, self.goal_status)
+
+
+    def goal_status(self, status, result):
+        """ Check the status of a goal - goal reached, aborted,
+        or rejected.
+        """
+        self.completion += 1
+
+        # Goal reached
+        if status == 3:
+            rospy.loginfo("Goal succeeded")
+
+        # Goal aborted
+        if status == 4:
+            rospy.loginfo("Goal aborted")
+
+        # Goal rejected
+        if status == 5:
+            rospy.loginfo("Goal rejected")
+
+
+    def check_neighbors(self, data, map_size):
+        """ Checks neighbors for random points on the map.
+        """
+        unknowns = 0
+        obstacles = 0
+
+        for x in range(-3, 4):
+            for y in range(-3, 4):
+                row = x * self.map_metadata.width + y
+                try:
+                    if data.data[map_size + row] == -1:
+                        unknowns += 1
+                    elif data.data[map_size + row] > 0.65:
+                        obstacles += 1
+                except IndexError:
+                    pass
+        if unknowns > 0 and obstacles < 2:
+            return True
+        else:
+            return False
+
+
+def main():
+    """ The main() function """
+    rospy.init_node('explore_node')
+    Explore()
+    rospy.spin()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("ExploreNode exception caught !!!")
---
+++ scripts/closedloop_nav_slam/ros/wheel_odometry_publisher.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+'''
+@file wheel_odometry_publisher.py
+@author Yanwei Du (yanwei.du@gatech.edu)
+@date 12-14-2023
+@version 1.0
+@license Copyright (c) 2023
+@desc None
+'''
+
+#! /usr/bin/env python
+import rospy
+from math import *
+import numpy as np
+from nav_msgs.msg import Odometry
+import tf
+from copy import deepcopy
+
+""" 
+The code is originally from https://blog.lxsang.me/post/id/16.0 with our modification. 
+The math theory locates https://docs.mrpt.org/reference/latest/tutorial-motion-models.html 
+"""
+
+
+class WheelOdometryPublisher:
+    def __init__(self):
+        self._alphas = [0.05, 0.001, 0.05, 0.05] # https://docs.mrpt.org/reference/latest/tutorial-motion-models.html
+        self._last_odom = None
+        self._last_disturbed_pose = None # [x, y, theta]
+
+        rospy.init_node("wheel_odometry_publisher_node")
+
+        # ROS params.
+        self._new_odom_frame = rospy.get_param("new_odom_frame", "new_odom")
+        self._base_frame = rospy.get_param("base_frame", "new_base_footprint")
+        self._enable_odom_to_base_tf = rospy.get_param("enable_odom_to_base_tf", True)
+        # Load noise parameters, default no noise.
+        # self._alphas = [rospy.get_param("alpha"+str(v), 0.0) for v in range(4)]
+
+        # ROS subscriber and publisher.
+        self._old_odom_sub = rospy.Subscriber("/odom_sparse", Odometry, self.__odom_callback)
+        self._new_odom_pub = rospy.Publisher("/visual/odom", Odometry,queue_size=1)
+
+        # Tf broadcaster
+        self._tf_br = tf.TransformBroadcaster()
+
+        rospy.spin()
+
+    def __odom_callback(self, msg):
+        if self._last_odom is None:
+            self._last_odom = msg
+            self._last_disturbed_pose = self.__convert_odom_to_pose_2d(msg)
+            return
+
+        new_odom = self.__compute_new_odom(self._last_odom, msg)
+
+        self._new_odom_pub.publish(new_odom)
+        if self._enable_odom_to_base_tf:
+            self._tf_br.sendTransform(
+                (new_odom.pose.pose.position.x, new_odom.pose.pose.position.y, new_odom.pose.pose.position.z,),
+                (new_odom.pose.pose.orientation.x,
+                 new_odom.pose.pose.orientation.y,
+                 new_odom.pose.pose.orientation.z,
+                 new_odom.pose.pose.orientation.w,),
+                new_odom.header.stamp,
+                self._base_frame,
+                self._new_odom_frame
+            )
+        self._last_odom = msg
+
+    def __convert_odom_to_pose_2d(self, odom):
+        quat = [
+            odom.pose.pose.orientation.x,
+            odom.pose.pose.orientation.y,
+            odom.pose.pose.orientation.z,
+            odom.pose.pose.orientation.w,
+        ]
+        return [
+            odom.pose.pose.position.x,
+            odom.pose.pose.position.y,
+            tf.transformations.euler_from_quaternion(quat)[-1] # [roll, pitch, yaw]
+        ]
+
+    def __compute_new_odom(self, last_odom, cur_odom):
+
+        theta1 = self.__convert_odom_to_pose_2d(last_odom)[-1]
+        theta2 = self.__convert_odom_to_pose_2d(cur_odom)[-1]
+        dx = cur_odom.pose.pose.position.x - last_odom.pose.pose.position.x
+        dy = cur_odom.pose.pose.position.y - last_odom.pose.pose.position.y
+
+        trans = np.sqrt(dx*dx + dy*dy)
+        rot1 = np.arctan2(dy, dx) - theta1
+        rot2 = theta2-theta1-rot1
+        a1, a2, a3, a4 = self._alphas
+        sd_rot1 = a1*np.abs(rot1) + a2*trans
+        sd_rot2 = a1*np.abs(rot2) + a2*trans
+        sd_trans = a3*trans + a4*(np.abs(rot1) + np.abs(rot2))
+ 
+        trans +=  np.random.normal(0,sd_trans*sd_trans)
+        rot1 += np.random.normal(0, sd_rot1*sd_rot1)
+        rot2 += np.random.normal(0, sd_rot2*sd_rot2)
+
+        self._last_disturbed_pose[0] += trans*np.cos(theta1+rot1)
+        self._last_disturbed_pose[1] += trans*np.sin(theta1+rot1)
+        self._last_disturbed_pose[2] += rot1 + rot2
+
+        new_odom = deepcopy(cur_odom)
+        new_odom.pose.pose.position.x = self._last_disturbed_pose[0]
+        new_odom.pose.pose.position.y = self._last_disturbed_pose[1]
+        quat = tf.transformations.quaternion_from_euler(0, 0, self._last_disturbed_pose[-1])
+        new_odom.pose.pose.orientation.x = quat[0]
+        new_odom.pose.pose.orientation.y = quat[1]
+        new_odom.pose.pose.orientation.z = quat[2]
+        new_odom.pose.pose.orientation.w = quat[3]
+        return new_odom
+
+if __name__ == "__main__":
+    try:
+        wop = WheelOdometryPublisher()
+    except rospy.ROSInterruptException:
+        rospy.loginfo("WheelOdometryPublisher: exception caught, the node must be terminated.")
