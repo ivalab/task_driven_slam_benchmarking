daac4ce6873a472cb691ff00e570dd021808d5e6
Yanwei Du <yanwei.du@gatech.edu>
2024-03-17 15:51:08 -0400

    Visual SLAM now works with path1

--- scripts/closedloop_nav_slam/settings/slam/call_svo.sh
+++ scripts/closedloop_nav_slam/settings/slam/call_svo.sh
@@ -7,6 +7,6 @@ cd ~/svo_ws
 source ~/svo_ws/devel/setup.bash
 
 # Launch svo with arguments $(num_feature) $(dataset) $(dir).
-LAUNCH_FILE='d435i_stereo.launch grid_size:='$1' dataset:='$2' trace_dir:='$3' slam_pose_topic:='$4
+LAUNCH_FILE='d435i_stereo_imu.launch grid_size:='$1' dataset:='$2' trace_dir:='$3' slam_pose_topic:='$4
 echo $LAUNCH_FILE
 roslaunch svo_ros $LAUNCH_FILE
